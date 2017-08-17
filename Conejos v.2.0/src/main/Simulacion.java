ec7492fc207e0d5212d7f1348db5d962f1a0523a
Daniel Pabon <danielpabon745@gmail.com>
2017-08-16 22:44:04 -0500

    VerificarMadurez --> LISTO!
    Metodo de verificación de madurez que cambia el estado a true.

--- Conejos v.2.0/src/main/Simulacion.java
+++ Conejos v.2.0/src/main/Simulacion.java
@@ -33,49 +33,55 @@ public class Simulacion {
 		}
 	}
 	
+	private void verificarMadurez(Conejo conejo) {
+		if (conejo.getEdad()>=conejo.getEdadMadurez()) {
+			conejo.setMadurez(true);
+		}
+	}
+	
 	
 	public void simular() {
 		//Bucle de los dias de estudio
 		for (int i = 0; i < tiempoEstudio; i++) {
 			//Bucle de conejos macho
 			for (int j = 0; j < machos.size(); j++) {
-				//--> Verificar la madurez del conejo: cambiar su estado a true en madurez.
+				verificarMadurez(machos.get(j));
 				machos.get(j).setEdad(machos.get(j).getEdad()+1);//Aumenta la edad del conejo.
-				//-->Verificar edad del conejo para matarlo. Edad de muerte (15 aÒos).
+				//-->Verificar edad del conejo para matarlo. Edad de muerte (15 a√±os).
 			}
 			//Bucle de conejos hembra
 			for (int j = 0; j < hembras.size(); j++) {
-				//--> Verificar la madurez del conejo: cambiar su estado a true en madurez.
-				//--> Calcular celo: depende de si ya madurÛ, si los dÌas de celo son iguales a 0 
-				//						y si no est· embarazada.
-				//--> Verificar celo: Si est· en celo: 
+				verificarMadurez(machos.get(j));
+				//--> Calcular celo: depende de si ya madur√≥, si los d√≠as de celo son iguales a 0 
+				//						y si no est√° embarazada.
+				//--> Verificar celo: Si est√° en celo: 
 				//		--> Mediante random decidir si queda embarazada o no.
 				//		--> Si queda embarazada: 
 				//				-Quitar celo,
-				//				-Generar el numero de gazapos: de 4 - 8 si la coneja tiene de 2 a 5 aÒos.
-				//				-Generar el tiempo de gestaciÛn de los conejos: 		
-				//							98% 31 dÌas --- 28 a 35 dÌas dependiendo del numero de gazapos.
+				//				-Generar el numero de gazapos: de 4 - 8 si la coneja tiene de 2 a 5 a√±os.
+				//				-Generar el tiempo de gestaci√≥n de los conejos: 		
+				//							98% 31 d√≠as --- 28 a 35 d√≠as dependiendo del numero de gazapos.
 				//		-->Sino queda: -Quitar celo
-				//					   -Calcular dÌas para el celo.
-				//-->Si est· embarazada:
+				//					   -Calcular d√≠as para el celo.
+				//-->Si est√° embarazada:
 				//		-->Verificar parto: Si los dias del parto son iguales a 0:
 				//					-->Calcular mortalidad de gazapos: 
 				//							en promedio 2, pero varia respecto a la
-				//							edad de la coneja, entre m·s joven m·s probabilidad de morir,
+				//							edad de la coneja, entre m√°s joven m√°s probabilidad de morir,
 				//							y la cantidad de gazapos en el parto, a mayor cantidad,
 				//							mayor probabilidad.
 				//					-->Generar cantidad de machos y hembras respecto a los sobrevivientes.
 				//					-->Agregar machos y hembras a la lista correspondiente.
 				//					-->Quitar estado de embarazo, poner estado de lactancia.
 				//					-->Calcular celo.
-				//					-->DÌas de lactancia: 4 semanas.
+				//					-->D√≠as de lactancia: 4 semanas.
 				//
-				//-->Si est· lactando:
-				//		-->Verificar lactancia y embarazo: Si la coneja tiene en 1 los dÌas de lactancia
-				//										y est· embarazada agregar 1 semana de lactancia.
+				//-->Si est√° lactando:
+				//		-->Verificar lactancia y embarazo: Si la coneja tiene en 1 los d√≠as de lactancia
+				//										y est√° embarazada agregar 1 semana de lactancia.
 				//
-				//-->Reducir dÌas: Metodo que reduzca los dÌas de todas los posibles estados 
-				//					siempre y cuando el numero de dÌas sea mayor a 0.
+				//-->Reducir d√≠as: Metodo que reduzca los d√≠as de todas los posibles estados 
+				//					siempre y cuando el numero de d√≠as sea mayor a 0.
 				//-->Aumentar edad coneja;
 			}
 		}
